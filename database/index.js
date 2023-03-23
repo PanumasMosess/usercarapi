86aeb926e81580479dc8eaa381999100a1a5c03a
Panumas Srisook <Panumas.srisook@gmail.com>
2023-03-23 17:13:01 +0700

    update port

--- database/index.js
+++ database/index.js
@@ -4,7 +4,8 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: 3306
 });
 
 module.exports = pool.promise();
