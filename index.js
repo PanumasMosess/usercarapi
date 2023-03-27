0404c0b5e37b4b62a57ec5b68e240d7ae8339638
Panumas Srisook <Panumas.srisook@gmail.com>
2023-03-27 14:46:42 +0700

    update cors function

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const {jwtValidate, jwtRefreshTokenValidate} = require('./token/jwtValidate')
 const carsRouter = require('./routes/cars.route')
 const tokenRouter = require('./routes/apiToken.route')
 
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
